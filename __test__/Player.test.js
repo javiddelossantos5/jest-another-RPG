0c708c44d0bfed282acd3fea19c74f9b9314e18d
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-03-23 14:01:20 -0600

    player.js is getting inventory and stats function finished

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,16 +1,21 @@
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player');
 jest.mock('../lib/Potion.js')
 
-const Player = require('../lib/Player');
-
-test('creates a player object', () => {
+test('creates a players stats as an object', () => {
     const player = new Player('Dave');
 
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-        );
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or return false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
